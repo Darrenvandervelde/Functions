407bd8c1008814a5e4207e810576a4c880375603
Darren van der Velde <darrenvandervelde@gmail.com>
2026-03-11 09:43:45 +0000

    Refactor debug functions for enhanced logging
    Replaced debug function with log, error, and warn functions for improved logging.

--- Models/Debug_Model.js
+++ Models/Debug_Model.js
@@ -1,26 +1,28 @@
+//------------------Debug Model------------------//
+
+// Pull Document File Name
 const fileName = window.location.pathname.split("/").pop();
-const DEBUG = true;
 
-// Debug function
-function debug(message) {
-    if (DEBUG) {
-        console.log("[DEBUG]:", message , " : " , fileName);
+// Debug if Enable Console logs all messages to the console.
+const Enable = true;
+
+// Log Use for loging Messages that have a state of run in it.
+function log(message) {
+    if(Enable === true) {
+        console.log("[DEBUG LOG]", message, fileName);
     }
 }
 
-// Example usage
-function testFunction() {
-
-    debug("Test function started");
-    debug("hello World")
-
-    const isActive = true;
-
-    if (isActive) {
-        debug("User is active");
+// Error Use for logging in most of the else statements if the code runs in a error or does not work. 
+function error(message) {
+    if(Enable === true) {
+        console.log("[DEBUG ERROR]", message, fileName );
     }
-
-    debug("Test function finished");
 }
 
-testFunction();
+// Warn Use for logging warning for System logic for version and software installed. 
+function warn(message) {
+    if (Enable === true) {
+        console.log("[DEBUG WARNING]", message, fileName)
+    }
+}
